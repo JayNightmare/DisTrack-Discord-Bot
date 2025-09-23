f8e37a5ff34d8b25baa7a028453987c3027dcd7e
Jay <jordanbell321@gmail.com>
2025-09-23 21:37:45 +0100

    Refactor website command to update URLs and improve code formatting

--- src/commands/website.js
+++ src/commands/website.js
@@ -1,65 +1,106 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require("@discordjs/builders");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('website')
-        .setDescription('Display the website!')
-        .addStringOption(option =>
-            option.setName('section')
-                .setDescription('Select a section you want to visit')
+        .setName("website")
+        .setDescription("Display the website!")
+        .addStringOption((option) =>
+            option
+                .setName("section")
+                .setDescription("Select a section you want to visit")
                 .setRequired(false)
                 .addChoices(
-                    { name: 'Showcase', value: 'https://distrack-website.vercel.app/showcase' },
-                    { name: 'Contact', value: 'https://distrack-website.vercel.app/contact' },
-                    { name: 'Download', value: 'https://distrack-website.vercel.app/temp' },
-                    { name: 'Source-Code', value: 'github'}
+                    {
+                        name: "Showcase",
+                        value: "https://distrack.nexusgit.info/showcase",
+                    },
+                    {
+                        name: "Contact",
+                        value: "https://distrack.nexusgit.info/contact",
+                    },
+                    {
+                        name: "Download",
+                        value: "https://distrack.nexusgit.info/temp",
+                    },
+                    { name: "Source-Code", value: "github" }
                 )
         ),
 
     async execute(interaction) {
         try {
             const targetUser = interaction.user;
-            const botAvatar = interaction.client.user.avatarURL({ size: 1024, dynamic: true }); // Fetches the bot's avatar in high resolution
+            const botAvatar = interaction.client.user.avatarURL({
+                size: 1024,
+                dynamic: true,
+            }); // Fetches the bot's avatar in high resolution
 
             let section;
             const embed = new EmbedBuilder()
-                .setTitle('Dis.Track Website')
-                .setDescription(`Hey <@${targetUser.id}>!\nCheck out the website for more information`)
+                .setTitle("Dis.Track Website")
+                .setDescription(
+                    `Hey <@${targetUser.id}>!\nCheck out the website for more information`
+                )
                 .setThumbnail(botAvatar)
-                
+
                 .setColor(0xf1c41d)
                 .setTimestamp();
 
-            const selectedSection = interaction.options.getString('section') || 'https://distrack-website.vercel.app';
+            const selectedSection =
+                interaction.options.getString("section") ||
+                "https://distrack.nexusgit.info";
 
-            if (selectedSection === "https://distrack-website.vercel.app/showcase") {
+            if (selectedSection === "https://distrack.nexusgit.info/showcase") {
                 section = "Showcase";
-                embed.addFields({ name: section, value: selectedSection, inline: true });
+                embed.addFields({
+                    name: section,
+                    value: selectedSection,
+                    inline: true,
+                });
                 embed.setURL(selectedSection);
-            } else if (selectedSection === "https://distrack-website.vercel.app/contact") {
+            } else if (
+                selectedSection === "https://distrack.nexusgit.info/contact"
+            ) {
                 section = "Contact";
-                embed.addFields({ name: section, value: selectedSection, inline: true });
+                embed.addFields({
+                    name: section,
+                    value: selectedSection,
+                    inline: true,
+                });
                 embed.setURL(selectedSection);
-            } else if (selectedSection === "https://distrack-website.vercel.app/temp") {
+            } else if (
+                selectedSection === "https://distrack.nexusgit.info/temp"
+            ) {
                 section = "Download";
-                embed.addFields({ name: section, value: selectedSection, inline: true });
+                embed.addFields({
+                    name: section,
+                    value: selectedSection,
+                    inline: true,
+                });
                 embed.setURL(selectedSection);
             } else if (selectedSection === "github") {
-                const extension = 'https://github.com/JayNightmare/DisTrack-VSCode-Extension';
-                const bot = 'https://github.com/JayNightmare/DisTrack-Discord-Bot';
-                const endpoint = 'https://github.com/JayNightmare/VSCode-Endpoint-Server-Deployment';
-                const website = 'https://github.com/JayNightmare/DisTrack-Website';
+                const extension =
+                    "https://github.com/JayNightmare/DisTrack-VSCode-Extension";
+                const bot =
+                    "https://github.com/JayNightmare/DisTrack-Discord-Bot";
+                const endpoint =
+                    "https://github.com/JayNightmare/VSCode-Endpoint-Server-Deployment";
+                const website =
+                    "https://github.com/JayNightmare/DisTrack-Website";
 
                 embed.addFields([
                     { name: "Extension", value: extension, inline: false },
                     { name: "Dis.Track Bot", value: bot, inline: false },
                     { name: "Endpoint API", value: endpoint, inline: false },
-                    { name: "Website", value: website, inline: false }
+                    { name: "Website", value: website, inline: false },
                 ]);
-                embed.setURL('https://github.com/JayNightmare');
+                embed.setURL("https://github.com/JayNightmare");
             } else {
                 section = "Home Page";
-                embed.addFields({ name: section, value: selectedSection, inline: true });
+                embed.addFields({
+                    name: section,
+                    value: selectedSection,
+                    inline: true,
+                });
                 embed.setURL(selectedSection);
             }
 
@@ -67,6 +108,5 @@ module.exports = {
         } catch (err) {
             console.error(err);
         }
-    }
-}
-
+    },
+};
